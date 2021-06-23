e1734475c1fbe97a32a98a5a82133a97fcbd7a7d
Aadam Ali <ali.aadam@outlook.com>
2021-06-23 20:02:12 +0100

    refactored constructor for class & changed styles

--- js/main.js
+++ js/main.js
@@ -1,128 +1,132 @@
 let myLibrary = [];
 
-function book(author, title, pages, readStatus) {
+// function book(author, title, pages, readStatus) {
+//     this.author = author;
+//     this.title = title;
+//     this.pages = pages;
+//     this.readStatus = readStatus;
+// };
+
+class Book {
+  constructor(author, title, pages, readStatus) {
     this.author = author;
     this.title = title;
     this.pages = pages;
     this.readStatus = readStatus;
-};
+  }
+}
 
-const newBookBtn = document.querySelector('.js-new-book-button');
-newBookBtn.addEventListener('click', openPopup);
+const newBookBtn = document.querySelector(".js-new-book-button");
+newBookBtn.addEventListener("click", openPopup);
 
-const addBookBtn = document.querySelector('.js-add-book-button');
-addBookBtn.addEventListener('click', addBook);
+const addBookBtn = document.querySelector(".js-add-book-button");
+addBookBtn.addEventListener("click", addBook);
 
-const popupForm = document.querySelector('.js-popup');
-popupForm.style.display = 'none';
+const popupForm = document.querySelector(".js-popup");
+popupForm.style.display = "none";
 
-const closePopupBtn = document.querySelector('.js-close-popup');
-closePopupBtn.addEventListener('click', closePopup);
+const closePopupBtn = document.querySelector(".js-close-popup");
+closePopupBtn.addEventListener("click", closePopup);
 
 function openPopup() {
-    popupForm.style.display = 'block';
-};
-
+  popupForm.style.display = "block";
+}
 
 function addBook() {
-    title = document.getElementById('title').value
-    author = document.getElementById('author').value
-    pages = document.getElementById('pages').value
-    readStatus = document.getElementById('read-status').checked
+  title = document.getElementById("title").value;
+  author = document.getElementById("author").value;
+  pages = document.getElementById("pages").value;
+  readStatus = document.getElementById("read-status").checked;
 
-    if (title == '' || author == '' || pages == ''){return}
-    
-    let newBook = new book(title, author, pages, readStatus)
-    myLibrary.push(newBook);
-    storeLibrary();
-    populateContainer();
-};
+  if (title == "" || author == "" || pages == "") {
+    return;
+  }
 
+  let newBook = new Book(title, author, pages, readStatus);
+  myLibrary.push(newBook);
+  storeLibrary();
+  populateContainer();
+}
 
 function storeLibrary() {
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-};
-
+  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
 
 function createBook(e) {
-    const container = document.querySelector('.container');
-    const bookContainer = document.createElement('div');
-    const titleDiv = document.createElement('div');
-    const authorDiv = document.createElement('div');
-    const pagesDiv = document.createElement('div');
-    const removeBtn = document.createElement('button');
-    const readBtn =  document.createElement('button');
+  const container = document.querySelector(".container");
+  const bookContainer = document.createElement("div");
+  const titleDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const pagesDiv = document.createElement("div");
+  const removeBtn = document.createElement("button");
+  const readBtn = document.createElement("button");
 
-    bookContainer.classList.add('book-container');
-    bookContainer.setAttribute('id', myLibrary.indexOf(e));
+  bookContainer.classList.add("book-container");
+  bookContainer.setAttribute("id", myLibrary.indexOf(e));
 
-    titleDiv.textContent = e.title;
-    titleDiv.classList.add('title');
-    bookContainer.appendChild(titleDiv);
+  titleDiv.textContent = e.title;
+  titleDiv.classList.add("title");
+  bookContainer.appendChild(titleDiv);
 
-    authorDiv.textContent = e.author;
-    authorDiv.classList.add('author');
-    bookContainer.appendChild(authorDiv);
+  authorDiv.textContent = e.author;
+  authorDiv.classList.add("author");
+  bookContainer.appendChild(authorDiv);
 
-    pagesDiv.textContent = e.pages;
-    pagesDiv.classList.add('pages');
-    bookContainer.appendChild(pagesDiv);
+  pagesDiv.textContent = e.pages;
+  pagesDiv.classList.add("pages");
+  bookContainer.appendChild(pagesDiv);
 
-    readBtn.classList.add('read-button');
-    bookContainer.appendChild(readBtn);
+  readBtn.classList.add("read-button");
+  bookContainer.appendChild(readBtn);
 
-    if (e.readStatus === false) {
-        readBtn.textContent = 'Not Read';
-        readBtn.style.backgroundColor = '#dd7777';
-    } else {
-        readBtn.textContent = 'Read';
-        readBtn.style.backgroundColor = '#77dd77';
-    }
+  if (e.readStatus === false) {
+    readBtn.textContent = "Not Read";
+    readBtn.style.backgroundColor = "#E94F37";
+  } else {
+    readBtn.textContent = "Read";
+    readBtn.style.backgroundColor = "#44BBA4";
+  }
 
-    removeBtn.textContent = 'Remove';
-    removeBtn.classList.add('remove-button');
-    bookContainer.appendChild(removeBtn);
+  removeBtn.textContent = "Remove";
+  removeBtn.classList.add("remove-button");
+  bookContainer.appendChild(removeBtn);
 
-    container.appendChild(bookContainer);
+  container.appendChild(bookContainer);
 
-    removeBtn.addEventListener('click', () => {
-        myLibrary.splice(myLibrary.indexOf(e), 1);
-        storeLibrary();
-        populateContainer();
-    });
+  removeBtn.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(e), 1);
+    storeLibrary();
+    populateContainer();
+  });
 
-    readBtn.addEventListener('click', () => { 
-        e.readStatus = !e.readStatus;
-        storeLibrary(); 
-        populateContainer();
-    }); 
-
-};
-
+  readBtn.addEventListener("click", () => {
+    e.readStatus = !e.readStatus;
+    storeLibrary();
+    populateContainer();
+  });
+}
 
 function closePopup() {
-    popupForm.style.display = 'none';
-};
-
+  popupForm.style.display = "none";
+}
 
 function populateContainer() {
-    const container = document.querySelector('.container');
-    container.textContent = '';
-    for (let i = 0; i < myLibrary.length; i++) {
-        createBook(myLibrary[i]);
-    };
-};
+  const container = document.querySelector(".container");
+  container.textContent = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    createBook(myLibrary[i]);
+  }
+}
 
-
-function retrieveLibrary () {
-    if(!localStorage.myLibrary) {
-        populateContainer();
-    } else {
-        let books = localStorage.getItem('myLibrary');
-        books = JSON.parse(books);
-        myLibrary = books;
-        populateContainer();
-    };
-};
+function retrieveLibrary() {
+  if (!localStorage.myLibrary) {
+    populateContainer();
+  } else {
+    let books = localStorage.getItem("myLibrary");
+    books = JSON.parse(books);
+    myLibrary = books;
+    populateContainer();
+  }
+}
 
 retrieveLibrary();
